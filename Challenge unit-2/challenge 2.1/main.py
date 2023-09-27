7ab9613802ba4a7009f17c58e8112ef97d08c0bd
Karthi <107454228+Karthicoc7075@users.noreply.github.com>
2023-09-27 06:56:14 +0530

    Add files via upload

---
+++ Challenge unit-2/challenge 2.1/main.py
@@ -0,0 +1,27 @@
+class BankAccount:
+  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
+  def deposit(self, amount):
+    if amount > 0:
+      self.__account_balance += amount
+      print("Deposited ₹{}. New balance: ₹{}".format(amount,self.__account_balance))
+    else:
+      print("Invalid deposit amount.")
+  def withdraw(self, amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance -= amount
+      print("Withdrew ₹{}. New balance: ₹{}".format(amount,self.__account_balance))
+    else:
+      print("Invalid withdrawal amount or insufficient balance.")
+  def display_balance(self):
+    print("Account balance for {} (Account #{}): ₹{}".format(self.__account_holder_name, self.__account_number,self.__account_balance))
+
+account = BankAccount(account_number="123456789",account_holder_name="Hari Prabu",initial_balance=5000.0)
+
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0)
+account.withdraw(20000.0)
+account.display_balance()
---
+++ Challenge unit-3/Challenge 3.1/main.py
@@ -0,0 +1,14 @@
+def linearSearchProduct(productList, targetProduct):
+  indices = []
+
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+
+  return indices
+
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
